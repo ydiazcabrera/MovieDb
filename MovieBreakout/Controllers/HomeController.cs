24ff1fe90028c116077780f58ad4864fb591a28f
Yeni <yen.diaz@hotmail.com>
2019-06-04 15:17:54 -0400

    first commit

---
+++ MovieBreakout/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using MovieBreakout.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieBreakout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Search(string title)
+        {
+            string APIText = GetAPIText(title);
+            Movie mov = ConvertToMovie(APIText);
+            return View(mov);
+        }
+
+        public string GetAPIText(string title)
+        {
+            string APIkey = "9dcc3c8a";
+            string URL = $"http://www.omdbapi.com/?apikey={APIkey}&&t={title}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(URL);
+
+            //There will sometimes be extra steps here between request and response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string APIText = rd.ReadToEnd();
+            return APIText;
+        }
+
+        public string GetListMovies(string title)
+        {
+            string APIkey ="9dcc3c8a";
+            string URL = $"http://www.omdbapi.com/?apikey={APIkey}&&s={title}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(URL);
+
+            //There will sometimes be extra steps here between request and response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string APIText = rd.ReadToEnd();
+            return APIText;
+        }
+
+        public Movie ConvertToMovie(string APIText)
+        {
+            JToken jsonData =JToken.Parse(APIText);
+            Movie m = new Movie();
+            m.Title = jsonData["Title"].ToString();
+            m.Plot = jsonData["Plot"].ToString();
+            m.PosterURL = jsonData["Poster"].ToString();
+            return m;
+        }
+        
+        public List<Movie> ConvertListOfMovies( string APIText)
+        {
+            JToken json = JToken.Parse(APIText);
+            List<JToken> filmToken = json["Search"].ToList();
+
+            List<Movie> MovieResults = new List<Movie>();
+
+            foreach(JToken j in filmToken)
+            {
+                Movie m = new Movie();
+                m.Title = j["Title"].ToString();
+
+                MovieResults.Add(m);
+            }
+            return MovieResults;
+
+        }
+        public ActionResult MovieList(string title)
+        {
+            string APIText = GetListMovies(title);
+            List<Movie> movies = ConvertListOfMovies(APIText);
+            return View(movies);
+        }
+    }
+}
